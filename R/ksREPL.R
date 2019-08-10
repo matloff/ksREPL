aba145d4030b6e61071aa97205a15119416150e4
matloff <matloff@cs.ucdavis.edu>
2019-08-09 22:36:09 -0700

    making pkg

--- R/ksREPL.R
+++ R/ksREPL.R
@@ -7,49 +7,48 @@
 ksInit <- function() 
 {
 
-
-lst <<- list(f = function() NA,prnt=TRUE)
-class(lst) <<- 'ksr'
-ksrProto <<- lst
-
-print.ksr <<- function(ksobj) {
-   if (ksobj$prnt) print(ksobj$f()) else ksobj$f()
-}
-
-'/.ksr' <<- function(ksobj,x) ksobj$f(x)
-
-# print(getwd())
-   gtd <<- ksrProto
-   gtd$f <<- getwd
-#  then 
-#     > gtd 
-#  prints current dir
-
-# setwd('..')
-   upd <<- ksrProto
-   upd$f <<- function() {setwd('..'); message(getwd())}
-   upd$prnt <<- FALSE
-#  then 
-#  > upd
-#  does cd ..
-
-# setwd(dir)
-#   could do
-#    std <<- ksrProto
-#    std$f <<- function(dir) {saveDir <<- dir; setwd(dir); message(getwd())}
-#    std$prnt <<- FALSE
-# then std/'xyz' to go to xyz dir, but doesn't save many keystrokes
-   std <<- function(dir) {
-       saveDir <<<- getwd()
-       setwd(dir)
+   lst <<- list(f = function() NA,prnt=TRUE)
+   class(lst) <<- 'ksr'
+   ksrProto <<- lst
+   
+   print.ksr <<- function(ksobj) {
+      if (ksobj$prnt) print(ksobj$f()) else ksobj$f()
    }
-#  then e.g.
-#  > std('abc')
-#  does cd to 'abc', saving current dir first
-
-# restore dir
-   bkd <<- ksrProto
-   bkd$f <<- function() {setwd(saveDir); saveDir <<- NULL; message(getwd())}
-   upd$prnt <<- FALSE
+   
+   '/.ksr' <<- function(ksobj,x) ksobj$f(x)
+   
+   # print(getwd())
+      gtd <<- ksrProto
+      gtd$f <<- getwd
+   #  then 
+   #     > gtd 
+   #  prints current dir
+   
+   # setwd('..')
+      upd <<- ksrProto
+      upd$f <<- function() {setwd('..'); message(getwd())}
+      upd$prnt <<- FALSE
+   #  then 
+   #  > upd
+   #  does cd ..
+   
+   # setwd(dir)
+   #   could do
+   #    std <<- ksrProto
+   #    std$f <<- function(dir) {saveDir <<- dir; setwd(dir); message(getwd())}
+   #    std$prnt <<- FALSE
+   # then std/'xyz' to go to xyz dir, but doesn't save many keystrokes
+      std <<- function(dir) {
+          saveDir <<- getwd()
+          setwd(dir)
+      }
+   #  then e.g.
+   #  > std('abc')
+   #  does cd to 'abc', saving current dir first
+   
+   # restore dir
+      bkd <<- ksrProto
+      bkd$f <<- function() {setwd(saveDir); saveDir <<- NULL; message(getwd())}
+      upd$prnt <<- FALSE
 
 }
